0fc2e3475049cf2d3c23396d46a82e834dc7380d
Zaklbird <rami.garah@outlook.com>
2024-09-20 17:12:40 +0100

    change test

--- app/page.tsx
+++ app/page.tsx
@@ -5,25 +5,10 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
         <div className={styles.ctas}>
           <a
             className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://www.facebook.com/garahrami"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -36,19 +21,11 @@ export default function Home() {
             />
             Rami
           </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
         </div>
       </main>
       <footer className={styles.footer}>
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://www.facebook.com/garahrami"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -59,7 +36,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn
+          haha
         </a>
         <a
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
@@ -73,7 +50,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Examples
+          xdxd
         </a>
         <a
           href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
@@ -87,7 +64,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          LOOOL →
         </a>
       </footer>
     </div>
